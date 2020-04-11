b6b85386e8a7a94aed44fbbaff242247bb369adf
Alexey Khodyrev <akhodyrev@sportmaster.ru>
2020-04-11 08:58:14 +0300

    подключение owner к TestNGTest поправил ворнинги от maven

--- tests-testng/src/test/java/TestNGTest.java
+++ tests-testng/src/test/java/TestNGTest.java
@@ -1,8 +1,11 @@
+import config.TestsConfig;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.aeonbits.owner.ConfigFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -12,12 +15,29 @@ public class TestNGTest {
 
     protected static WebDriver driver;
     private Logger logger = LogManager.getLogger(TestNGTest.class);
+    private TestsConfig cfg = ConfigFactory.create(TestsConfig.class);
+
+    private final String chrome = "chrome";
+    private final String firefox = "firefox";
 
     @BeforeTest
     private void setUp(){
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        logger.info("Поднят драйвер Chrome!");
+        switch (cfg.browser().toLowerCase()){
+            case chrome: {
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                logger.info("Поднят драйвер Chrome!");
+                break;
+            }
+            case firefox: {
+                WebDriverManager.firefoxdriver();
+                driver = new FirefoxDriver();
+                logger.info("Поднят драйвер Firefox!");
+                break;
+            }
+            default:
+                throw new IllegalStateException("Unexpected value: " + cfg.browser().toLowerCase());
+        }
     }
 
     @Test
